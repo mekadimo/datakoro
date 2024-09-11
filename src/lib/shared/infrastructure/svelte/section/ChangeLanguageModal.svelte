3dda80cba5b640fafcdc8fb0df9d5c6a58ed0f93
Lajto <85128868+lajtomekadimon@users.noreply.github.com>
2024-09-11 17:06:23 +0000

    Fix rendering of wrong page.

--- src/lib/shared/infrastructure/svelte/section/ChangeLanguageModal.svelte
+++ src/lib/shared/infrastructure/svelte/section/ChangeLanguageModal.svelte
@@ -4,7 +4,7 @@
 
     let { i18n } = AppGlobalState.get();
 
-    export let isOpen;
+    export let isOpen: boolean;
     export let hideLanguageModal: () => void;
 
     const SUPPORTED_LANGUAGES = [
@@ -12,16 +12,18 @@
         { code: "es", originalName: "Español" },
     ];
 
-    function i18nUrl(languageCode: string): string {
-        const url = $page.url.pathname;
+    function i18nUrl(languageCode: string, currentPage: typeof $page): string {
+        const url = currentPage.url.pathname;
         const regex = /^\/[a-z]+(\/|$)/;
         const newUrl =
-            url.replace(regex, `/${languageCode}$1`) + $page.url.search;
+            url.replace(regex, `/${languageCode}$1`) + currentPage.url.search;
         return newUrl;
     }
 </script>
 
 <div class="modal" class:is-active={isOpen}>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
     <div class="modal-background" on:click={hideLanguageModal}></div>
     <div class="modal-card">
         <header class="modal-card-head">
@@ -43,7 +45,7 @@
                             <a
                                 class:is-active={supportedLanguage.code ===
                                     $i18n.language}
-                                href={i18nUrl(supportedLanguage.code)}
+                                href={i18nUrl(supportedLanguage.code, $page)}
                             >
                                 {supportedLanguage.originalName}
                                 {#if supportedLanguage.code !== $i18n.language}
